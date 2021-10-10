79c303dc0ce3b77861c2c9a2ce55ba54b30f1324
junman95 <junman95@naver.com>
2021-10-11 00:37:33 +0900

    [Add] Each sections call up firebase data as a thumbnail

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,25 +2,66 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
 import Viewers from "./Viewers";
-import Movies from "./Movies";
+import Recommend from "./Recommend";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Trending from "./Trending";
 //database
 import db from "../firebase";
+//redux
+import { useDispatch, useSelector } from "react-redux";
+import { selectUserName } from "../features/user/userSlice";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectRecommend } from "../features/movie/movieSlice";
 
-function Home() {
+const Home = (props) => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trendings = [];
+
   useEffect(() => {
-    db.collection("movies").onSnapshot((snap)=>{
-      console.log(snap);
-    })
-  }, [])
-
+    db.collection("movies").onSnapshot((snap) => {
+      snap.docs.map((doc) => {
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+          case "trending":
+            trendings = [...trendings, { id: doc.id, ...doc.data() }];
+            break;
+          default:
+        }
+      });
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trendings,
+        })
+      );
+    });
+  }, [userName]);
   return (
     <Container className="homepage">
       <ImageSlider />
       <Viewers />
-      <Movies />
+      <Recommend />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   );
-}
+};
 
 export default Home;
 
